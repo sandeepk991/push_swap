ed29dd2462a11c7bf098641cc04d532524f29c59
sandy <sandeep.kaur@gmx.com>
2023-06-06 17:50:09 +0200

    updated

---
+++ push_swap.h
@@ -0,0 +1,54 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+
+// Stack
+typedef struct s_list
+{
+	int				data;
+	int				index_value;
+	struct s_list	*next;
+}				t_list;
+
+t_list	*ft_lstnew(int data);
+t_list	*ft_lstlast(t_list *head);
+void	ft_lstadd_front(t_list **stack, t_list *new);
+void	ft_lstadd_back(t_list **stack, t_list *new);
+int		ft_lstsize(t_list *head);
+
+void	ft_error(char *msg);
+void	check_args(int argc, char **argv);
+int		is_stack_sorted(t_list **stack);
+int		get_distance(t_list **stack, int index);
+void	free_stack(t_list **stack);
+void	ft_free(char **str);
+
+void	radix_sort(t_list **stack_a, t_list **stack_b);
+//void	simple_sort(t_list **stack_a, t_list **stack_b);
+void	index_stack(t_list **stack);
+void	sort_2_args(t_list **stack_a);
+void	sorting(t_list **stack_a, t_list **stack_b);
+//static void	sort_3_args(t_list **stack_a);
+//static void	sort_4_args(t_list **stack_a, t_list **stack_b);
+void	sort_3(t_list **stack_a, t_list *head, int min, int next_min);
+void	sort_5_args(t_list **stack_a, t_list **stack_b);
+
+int		ft_swap(t_list **stack_a);
+int		ft_push(t_list **stack_a, t_list **stack_b);
+int		ft_rotate(t_list **stack_a);
+int		ft_reverseRotate(t_list **stack_a);
+
+int		ft_sa(t_list **stack_a);
+int		ft_sb(t_list **stack_b);
+int		ft_ss(t_list **stack_a, t_list **stack_b);
+int		ft_pa(t_list **stack_a, t_list **stack_b);
+int		ft_pb(t_list **stack_b, t_list **stack_a);
+int		ft_ra(t_list **stack_a);
+int		ft_rb(t_list **stack_b);
+int		ft_rr(t_list **stack_a, t_list **stack_b);
+int		ft_rra(t_list **stack_a);
+int		ft_rrb(t_list **stack_b);
+int		ft_rrr(t_list **stack_a, t_list **stack_b);
+
+#endif
